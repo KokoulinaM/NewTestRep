63225b5bcd8fcc9e985f7d9807294afcf8b110db
KokoulinaM <Foririshka@yandex.ru>
2019-04-15 20:55:19 +0300

    гружуфсе

--- final.py
+++ final.py
@@ -1,59 +1,73 @@
 import random as rend
 import re
 import logging as lg
+import json
+
+streets = []
+lg.basicConfig(filename="finish.log", level=lg.INFO)
+
+with open('streets.txt', 'rt') as f:
+    data = f.read()
 
 
-def my_decor(dec_foo):
-    """
-    Если функция принимает хотя бы один параметром со значением my_decor.strTest, то не выполняем
-    :param dec_foo:
-    :return: результат выполнения функции
-    """
-    my_decor.strTest = "Тест"
+try:
+    streets = json.loads(data)["Streets"]
+except json.JSONDecodeError as e:
+    pass
 
-    def wr(*args, **kwargs):
-        if any(x == my_decor.strTest for x in args):
-            print(f"Вызов функции  {dec_foo.__name__}  с параметром {args}")
-        else:
-            result = dec_foo(*args, **kwargs)
-            return result
-    return wr
+def decor(strTest):
+
+    def my_decor(dec_foo):
+        """
+        Если функция принимает хотя бы один параметром со значением my_decor.strTest, то не выполняем
+        :param dec_foo:
+        :return: результат выполнения функции
+        """
+
+        def wr(*args, **kwargs):
+            if strTest in args or strTest in kwargs.values():
+                print(f"Вызов функции  {dec_foo.__name__}  с параметром {args}")
+            else:
+                result = dec_foo(*args, **kwargs)
+                return result
+        return wr
+    return my_decor
 
 
-def check_street(street):
+def check_street():
     """
     Функция проверяет корректность названия улиц
-    :param street: Название предположительной улицы
-    :return: Если прошла проверку, то True
+
     """
-    if re.findall(r'[\W|\d]', street).__len__() > 0:
-        lg.basicConfig(filename="finish.log", level=lg.INFO)
-        lg.warning(street + " - улица не прошла проверку")
-        return False
-    else:
-        return True
+    for street in streets:
+        if re.findall(r'[\W|\d]', street):
+            lg.warning(street + " - улица не прошла проверку")
+            streets.remove(street)
 
 
-@my_decor
-def address_gen(typef: str = '') -> str:
+@decor("Test")
+def address_gen(typef=''):
     """
      Функция генерации случайных адресов
     """
     while True:
         rstreet = rend.choice(streets)
-        if check_street(rstreet):
-            return country + ', ' + city + ', ' + rstreet + ', ' + str(rend.randint(1, 255)) + '-' \
-                   + str(rend.randint(1, 2000)) + ' ' + typef
-        else:
-            break
+        yield country + ', ' + city + ', ' + rstreet + ', ' + str(rend.randint(1, 255)) + rend.choice(lit) +\
+            '-' + str(rend.randint(1, 2000))
 
 
 if __name__ == '__main__':
     country = 'Russia'
     city = 'Spb'
-    streets = ('Ordjonikidze', 'Lensoveta//g', 'Nevsky', 'Bogatirsky', 'Novaya-f', 'Cvetochnaya', 'Tipanova')
+    lit = ('', 'a')
+    check_street()
 
+    adr=address_gen()
     for i in range(25):
-        print(address_gen(''))
+        print(next(adr))
 
-    address_gen('Тест')
+address_gen("Test")
+
+# json input data
+# streets check once
+# decorator
